838f36e776980f9c469690d83fac79914f7cc870
Ryan Adams <ryanadams14@gmail.com>
2021-08-11 23:05:52 -0500

    reformating website for blog

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const {
+  BlogPost
+} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newBlogPost = await BlogPost.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newBlogPost);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +19,21 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const blogData = await BlogPost.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!blogData) {
+      res.status(404).json({
+        message: 'No project found with this id!'
+      });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(blogData);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,33 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const {
+  BlogPost,
+  User
+} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const blogData = await BlogPost.findAll({
+      attributes: {
+        include: ['title', 'body', 'date_created']
+      },
+    }, {
+      include: [{
+        model: User,
+        attributes: ['name'],
+      }, ],
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const blogs = blogData.map((blog) => blog.get({
+      plain: true
+    }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -30,15 +37,15 @@ router.get('/', async (req, res) => {
 router.get('/project/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+      include: [{
+        model: User,
+        attributes: ['name'],
+      }, ],
     });
 
-    const project = projectData.get({ plain: true });
+    const project = projectData.get({
+      plain: true
+    });
 
     res.render('project', {
       ...project,
@@ -54,11 +61,17 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model: BlogPost
+      }],
     });
 
-    const user = userData.get({ plain: true });
+    const user = userData.get({
+      plain: true
+    });
 
     res.render('profile', {
       ...user,
